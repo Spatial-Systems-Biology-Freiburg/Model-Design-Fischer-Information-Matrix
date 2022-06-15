263bdcfb3d1e3a2fa612b09c169fa09c9698ffb0
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2022-06-15 19:29:28 +0200

    attempt to multirehad it.

--- growth_model.py
+++ growth_model.py
@@ -4,6 +4,8 @@ import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 import itertools as iter
+from multiprocessing import Pool
+from functools import partial
 
 
 def ODE_RHS(n, t, a, b, n_max, temp):
@@ -46,23 +48,41 @@ def get_large_S_matrix(params, variables, results):
     return S
 
 
+def Large_S_matrix_to_determinant(S, params, variables, sample_times):
+    S_trans = S.reshape(len(params), sum(y.size for y in variables) * sample_times.size)
+    
+    # Calculate Fisher Matrix
+    F = S_trans.dot(S_trans.T)
+    
+    # Calculate Eigenvalues and determinant
+    # w, v = np.linalg.eig(F)
+    det = np.linalg.det(F)
+    return det
+
+
+def Large_S_matrix_to_eigenvalue(S, params, variables, sample_times):
+    S_trans = S.reshape(len(params), sum(y.size for y in variables) * sample_times.size)
+    
+    # Calculate Fisher Matrix
+    F = S_trans.dot(S_trans.T)
+    
+    # Calculate Eigenvalues and determinant
+    w, v = np.linalg.eig(F)
+    return w
+
+
+def calculate_Fischer_determinant(sample_times, ODE_func, params, variables):
+    results = obtain_results(ODE_func, params, variables, sample_times)
+    S = get_large_S_matrix(params, variables, results)
+    det = Large_S_matrix_to_determinant(S, params, variables, sample_times)
+    return det, sample_times, params, variables
+
+
 if __name__ == "__main__":
     # Choose literature values
     a_0 = 1.0
     b_0 = 2.0
     n_max_0 = 1000
-
-    # Define t-values
-    t0 = 0.0
-    tmax = 0.4
-    dt = 0.05
-
-    # Just try for single temperature and initial value for n
-    n0 = 20
-
-    # Define sample space
-    sample_temps = np.arange(20, 30, 2.5)
-    sample_times = np.arange(t0, tmax, dt)
 
     # This is just to calculate the differential at the point
     diff_low = 0.9
@@ -82,22 +102,42 @@ if __name__ == "__main__":
         sample_n_max,
     ]
 
+    # Define t-values
+    t0 = 0.0
+    tmax = 0.4
+
+    # Just try for single temperature and initial value for n
+    n0 = 20
+
+    # Define sample space
+    sample_temps = np.arange(20, 30, 0.1)
+
     # # Additional Variables (like Temperature) over which we want to iterate
     variables = [
         sample_temps
     ]
 
-    results = obtain_results(ODE_RHS, params, variables, sample_times)
+    many_sample_times = [np.sort(np.random.uniform(low=t0, high=tmax, size=n)) for n in range(2, 5)]
+    results = []
 
-    S = get_large_S_matrix(params, variables, results)
-    
-    S_trans = S.reshape(len(params), sum(y.size for y in variables) * sample_times.size)
-    
-    # Calculate Fisher Matrix
-    F = S_trans.dot(S_trans.T)
-    
-    # Calculate Eigenvalues and determinant
-    w, v = np.linalg.eig(F)
-    det = np.linalg.det(F)
+    with Pool(3) as p:
+        results = p.map(partial(calculate_Fischer_determinant, ODE_func=ODE_RHS, params=params, variables=variables), many_sample_times)
 
-    print(det)
+    # for sample_times in many_sample_times:
+    #     results.append(calculate_Fischer_determinant(sample_times, ODE_RHS, params, variables))
+
+    results = sorted(results, key=lambda x: x[0], reverse=True)
+    
+    print("The first 4 winners are:")
+    for det, times, param, var in results[0:5]:
+        print("det:", det)
+        print("Times: ", times)
+        print("Parameter Values: ", param)
+        print("Variable Values: ", var)
+
+    for det, times, param, var in results[0:5]:
+        plt.plot(times, times*0.0, marker="o", linestyle="", label="Det: " + str(det))
+    plt.legend()
+    plt.show()
+    # pool = Pool(20)
+    # pool.map()
