Correct the mistake with ode function/ add t0 for ODE calculation

pool_model.py
@@ -12,6 +12,7 @@ from pool_model_plots import make_nice_plot, make_convergence_plot, make_plots, 
 from src.database import convert_fischer_results, generate_new_collection, insert_fischer_dataclasses, drop_all_collections
 
 
+
 def pool_model_sensitivity(y, t, Q, P, Const):
     (a, b, c) = P
     (Temp,) = Q
@@ -25,7 +26,7 @@ def pool_model_sensitivity(y, t, Q, P, Const):
     ]
 
 
-def jacobi(t, y, Q, P, Const):
+def jacobi(y, t, Q, P, Const):
     (n, sa, sb, sc) = y
     (a, b, c) = P
     (Temp,) = Q
@@ -74,7 +75,7 @@ if __name__ == "__main__":
     P = (a, b, c)
 
     # Initial values for complete ODE (with S-Terms)
-    y0 = np.array([n0, 0, 0, 0])
+    y0 = [n0, 0, 0, 0]
 
     # Define bounds for sampling
     temp_low = 2.0
@@ -88,6 +89,9 @@ if __name__ == "__main__":
     dtimes = 1.0
     n_times_max = int((times_high-times_low) / dtimes + 1) # effort+1
     times_total = np.linspace(times_low, times_low + dtimes * (n_times_max - 1), n_times_max)
+
+    # Initial conditions with initial time
+    y0_t0 = (y0, times_low)
 
     # How often should we choose a sample with same number of temperatures and times
     N_mult = 250
@@ -129,7 +133,7 @@ if __name__ == "__main__":
         fischer_results = p.starmap(calculate_Fischer_observable, zip(
             combinations,
             iter.repeat(pool_model_sensitivity),
-            iter.repeat(y0),
+            iter.repeat(y0_t0),
             iter.repeat(jacobi),
             iter.repeat(convert_S_matrix_to_determinant)
         ))

src/solving.py
@@ -15,13 +15,14 @@ def factorize_reduced(M):
     return res
 
 
-def get_S_matrix(ODE_func, y0, times, Q_arr, P, Const, jacobian):
+def get_S_matrix(ODE_func, y0_t0, times, Q_arr, P, Const, jacobian):
     """now we calculate the derivative with respect to the parameters
     The matrix S has the form
     i   -->  index of parameter
     jk  -->  index of kth variable
     t   -->  index of time
     S[i, j1, j2, ..., t] = (dO/dp_i(v_j1, v_j2, v_j3, ..., t))"""
+    (y0, t0) = y0_t0
     S = np.zeros((len(P),) + (times.shape[-1],) + tuple(len(x) for x in Q_arr))
 
     # Iterate over all combinations of Q-Values
@@ -31,7 +32,8 @@ def get_S_matrix(ODE_func, y0, times, Q_arr, P, Const, jacobian):
         t = times[index]
 
         # Actually solve the ODE for the selected parameter values
-        r = odeint(ODE_func, y0, t, args=(Q, P, Const), Dfun=jacobian).T[1:,:]
+        #r = solve_ivp(ODE_func, [t0, t.max()], y0, method='Radau', t_eval=t,  args=(Q, P, Const), jac=jacobian).y.T[1:,:]
+        r = odeint(ODE_func, y0, np.insert(t, 0, t0), args=(Q, P, Const), Dfun=jacobian).T[1:,1:]
 
         # Calculate the S-Matrix with the supplied jacobian
         S[(slice(None), slice(None)) + index] = r
@@ -71,4 +73,3 @@ def calculate_Fischer_observable(combinations, ODE_func, Y0, jacobian, observabl
     S = get_S_matrix(ODE_func, Y0, times, Q_arr, P, Const, jacobian)
     obs = observable(S)
     return obs, times, P, Q_arr, Const, Y0
-