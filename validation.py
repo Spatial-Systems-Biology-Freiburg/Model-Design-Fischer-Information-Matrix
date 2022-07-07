8608d5464eb07faf96e43de92fae05066ca8be9e
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2022-07-07 14:11:26 +0200

    validation: restore function to get_S_matrix

--- validation.py
+++ validation.py
@@ -129,6 +129,31 @@ def plot_odes(ODE_func, y0_t0, times, Q_arr, P, Const, jacobian=None):
             ax[j].legend()
     fig.tight_layout()
     fig.savefig("plots/validation.png")
+
+
+def get_S_matrix(ODE_func, y0_t0, times, Q_arr, P, Const, jacobian=None):
+    """now we calculate the derivative with respect to the parameters
+    The matrix S has the form
+    i   -->  index of parameter
+    jk  -->  index of kth variable
+    t   -->  index of time
+    S[i, j1, j2, ..., t] = (dO/dp_i(v_j1, v_j2, v_j3, ..., t))"""
+    (y0, t0) = y0_t0
+    S = np.zeros((len(P),) + (times.shape[-1],) + tuple(len(x) for x in Q_arr))
+
+    # Iterate over all combinations of Q-Values
+    for index in it.product(*[range(len(q)) for q in Q_arr]):
+        # Store the results of the respective ODE solution
+        Q = [Q_arr[i][j] for i, j in enumerate(index)]
+        t = times[index]
+
+        # Actually solve the ODE for the selected parameter values
+        #r = solve_ivp(ODE_func, [t0, t.max()], y0, method='Radau', t_eval=t,  args=(Q, P, Const), jac=jacobian).y.T[1:,:]
+        r = odeint(ODE_func, y0, np.insert(t, 0, t0), args=(Q, P, Const), Dfun=jacobian)
+        q = r.T[1:,1:]
+
+        # Calculate the S-Matrix with the supplied jacobian
+        S[(slice(None), slice(None)) + index] = q
     
     # Reshape to 2D Form (len(P),:)
     S = S.reshape((len(P),np.prod(S.shape[1:])))
@@ -198,6 +223,7 @@ if __name__ == "__main__":
 
     ode = pool_model_sensitivity
     plot_odes(ode, y0_t0, times, [temperatures], P, Const, jacobian=jacobi)
+    S = get_S_matrix(ode, y0_t0, times, [temperatures], P, Const, jacobian=jacobi)
 
     # Print the fischer matrix and its eigenvalues and determinant
     F = S.dot(S.T)
