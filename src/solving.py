1bb69bf899c1afa962855ea942df045c31d4d88e
polinagaindrik <polinagayndrik@gmail.com>
2022-07-19 10:37:00 +0300

    Add choice of error covariance matrix (relative/absolute error, without error or give matrix manually)

--- src/solving.py
+++ src/solving.py
@@ -11,8 +11,8 @@ def factorize_reduced(M):
             res.append((i, round(M/i)))
     return res
 
-
-def get_S_matrix(ODE_func, y0_t0, times, Q_arr, P, Const, jacobian=None):
+                                                                        #  err = relat_err/ abs_err/ cov_matrix or (abs_err, relat_err) for diff methods
+def get_S_matrix(ODE_func, y0_t0, times, Q_arr, P, Const, jacobian=None, method='wo_error', err=None):
     """now we calculate the derivative with respect to the parameters
     The matrix S has the form
     i   -->  index of parameter
@@ -21,7 +21,7 @@ def get_S_matrix(ODE_func, y0_t0, times, Q_arr, P, Const, jacobian=None):
     S[i, j1, j2, ..., t] = (dO/dp_i(v_j1, v_j2, v_j3, ..., t))"""
     (y0, t0) = y0_t0
     S = np.zeros((len(P),) + (times.shape[-1],) + tuple(len(x) for x in Q_arr))
-    error_n = np.zeros((times.shape[-1],) + tuple(len(x) for x in Q_arr))
+    n_solution = np.zeros((times.shape[-1],) + tuple(len(x) for x in Q_arr))
 
     # Iterate over all combinations of Q-Values
     for index in iter.product(*[range(len(q)) for q in Q_arr]):
@@ -35,13 +35,24 @@ def get_S_matrix(ODE_func, y0_t0, times, Q_arr, P, Const, jacobian=None):
 
         # Calculate the S-Matrix with the supplied jacobian
         S[(slice(None), slice(None)) + index] = r[1:]
+        n_solution[:, index] = r[0].reshape(times.shape[-1], 1)
 
-        # Assume that the error of the measurement is 25% from the measured value r[0] n 
-        # (use for covariance matrix calculation)
-        error_n[:, index] = r[0].reshape(times.shape[-1], 1) * 0.25
     # Reshape to 2D Form (len(P),:)
     S = S.reshape((len(P),np.prod(S.shape[1:])))
-    error_n = error_n.reshape(np.prod(error_n.shape))
+    # Calculate covariance matrix
+    if method == 'manual':
+        cov_matrix = err
+    else:
+        cov_matrix = np.eye(times.size, times.size)
+        error_n = np.zeros((times.shape[-1],) + tuple(len(x) for x in Q_arr))
+        for index in iter.product(*[range(len(q)) for q in Q_arr]):
+            if method == 'relative_error':
+                error_n[:, index] =  n_solution[:, index] * err
+            elif method == 'absolute_error':
+                error_n[:, index] =  err
+            elif method == 'combined_error':
+                error_n[:, index] =  err[0] + n_solution[:, index] * err[1]
+        error_n = error_n.reshape(np.prod(error_n.shape))
     cov_matrix = np.eye(len(error_n), len(error_n)) * error_n**2
     C = np.linalg.inv(cov_matrix)
     return S, C
@@ -57,7 +68,7 @@ def convert_S_matrix_to_determinant(times, Q_arr, P, Const, S, C):
     return det
 
 
-def convert_S_matrix_to_sumeigenval(S, C):
+def convert_S_matrix_to_sumeigenval(times, Q_arr, P, Const, S, C):
     # Calculate Fisher Matrix
     F = S.dot(C).dot(S.T)
 
@@ -65,7 +76,7 @@ def convert_S_matrix_to_sumeigenval(S, C):
     sumeigval = np.sum(np.linalg.eigvals(F))
     return sumeigval
 
-def convert_S_matrix_to_mineigenval(S, C):
+def convert_S_matrix_to_mineigenval(times, Q_arr, P, Const, S, C):
     # Calculate Fisher Matrix
     F = S.dot(C).dot(S.T)
 
@@ -74,10 +85,11 @@ def convert_S_matrix_to_mineigenval(S, C):
     return mineigval
 
 
-def calculate_Fischer_observable(combinations, ODE_func, Y0, jacobian, observable, covar=False):
-    times, Q_arr, P, Const = combinations
-    S, C = get_S_matrix(ODE_func, Y0, times, Q_arr, P, Const, jacobian)
-    if covar == False:
-        C = np.eye(S.shape[1])
+def calculate_Fischer_observable(combinations, ODE_func, Y0, jacobian, observable, method='wo_error', err=None):
+    # methods = 'wo_error', 'relative_error', 'absolute_error', 'combined_error'
+    times, P, Q_arr, Const = combinations
+    if method != 'wo_error' and err == None:
+        print('Error: Argument "err" should be provided')
+    S, C = get_S_matrix(ODE_func, Y0, times, Q_arr, P, Const, jacobian, method, err)
     obs = observable(times, Q_arr, P, Const, S, C)
     return obs, times, P, Q_arr, Const, Y0
