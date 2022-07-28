71f930c9df23c376b2276232083a0d29e909ded3
polinagaindrik <polinagayndrik@gmail.com>
2022-07-28 18:12:14 +0300

    Simplify the structure of the fischer_results read from collection

--- src/database.py
+++ src/database.py
@@ -117,5 +117,5 @@ def list_all_collections():
 
 def get_fischer_results_from_collection(collection):
     coll = get_collection(collection)
-    fisses = [[[revert_marks(c[key]) for key in ["observable", "times", "parameters", "q_arr", "constants", "y0"]]] for c in coll.find()]
+    fisses = [[revert_marks(c[key]) for key in ["observable", "times", "parameters", "q_arr", "constants", "y0"]] for c in coll.find()]
     return fisses
